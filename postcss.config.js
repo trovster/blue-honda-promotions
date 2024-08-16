98bb0d4e33232ae4fedbcd1a8149ee73b6db4b38
Trevor Morris <code@trovster.com>
2024-08-16 17:42:30 +0100

    fix: minify css for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 import tailwindcss from "tailwindcss"
 import autoprefixer from "autoprefixer"
+import cssnano from "cssnano"
 
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
 }
