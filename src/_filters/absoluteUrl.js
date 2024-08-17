4dc42d58a77c6820c4428e29d0e4940422dc9b29
Trevor Morris <code@trovster.com>
2024-08-17 19:13:03 +0100

    fix: remove absoluteUrl filter and use the built in base with path prefix

--- src/_filters/absoluteUrl.js
+++
@@ -1,9 +0,0 @@
-import { URL } from "url"
-
-export default (url, base) => {
-    try {
-        return new URL(url.replace(/^\//, ""), base).toString()
-    } catch (e) {
-        return url
-    }
-}
--- src/_filters/index.js
+++ src/_filters/index.js
@@ -1,10 +1,8 @@
 import date from "./date.js"
-import absoluteUrl from "./absoluteUrl.js"
 import { upcoming, past, exclude, pluck, next, limit } from "./collections.js"
 
 export default {
     date,
-    absoluteUrl,
     upcoming,
     past,
     exclude,
