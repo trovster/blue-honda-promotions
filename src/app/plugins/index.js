7aadfa2997f1c2bf72081339228ef7ff9f0373bd
Trevor Morris <code@trovster.com>
2024-08-19 17:35:24 +0100

    fix: navigation based on plugin

--- src/app/plugins/index.js
+++ src/app/plugins/index.js
@@ -1,5 +1,7 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy"
+import EleventyNavigationPlugin from "@11ty/eleventy-navigation"
 
 export default {
     EleventyHtmlBasePlugin,
+    EleventyNavigationPlugin,
 }
