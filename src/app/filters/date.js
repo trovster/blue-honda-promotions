d31689ab635aae5b27e1c09481ff56f3e972e747
Trevor Morris <code@trovster.com>
2024-08-19 16:36:39 +0100

    fix: add ordinal to date filter

--- src/app/filters/date.js
+++ src/app/filters/date.js
@@ -1,7 +1,16 @@
 import { DateTime } from "luxon"
 
+// @see https://github.com/moment/luxon/issues/118
+// @uses 'S' from php.net/date
+function ordinal(n) {
+    var s = ["th", "st", "nd", "rd"]
+    var v = n % 100
+    return "'" + n + (s[(v - 20) % 10] || s[v] || s[0]) + "'"
+}
+
 /**
  * Format a date with Luxon.
+ * If you provide ddS in the format, it assumes you want the ordinal suffix.
  *
  * @param {String} date - string Date
  * @param {String} format - date format (Luxon)
@@ -9,5 +18,6 @@ import { DateTime } from "luxon"
  * @returns {String} formatted date
  */
 export default (date, format, locale = "en") => {
-    return DateTime.fromISO(date).setLocale(locale).toFormat(format)
+    date = DateTime.fromISO(date).setLocale(locale)
+    return date.toFormat(format.replace('ddS', ordinal(date.day)))
 }
