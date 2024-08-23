1f39b45d6f633ca75c2edc7e660340976b14f2af
Trevor Morris <code@trovster.com>
2024-08-23 12:15:35 +0100

    fix: simplify site data

--- src/data/site.js
+++ src/data/site.js
@@ -3,11 +3,9 @@ import { createRequire } from "node:module"
 const require = createRequire(import.meta.url)
 const pkg = require("../../package.json")
 
-export default () => {
-    return {
-        title: pkg.description, // process.env.npm_package_config_title
-        description: process.env.npm_package_config_description,
-        keywords: pkg.keywords,
-        url: process.env.URL,
-    }
+export default {
+    title: pkg.description, // process.env.npm_package_config_title
+    description: process.env.npm_package_config_description,
+    keywords: pkg.keywords,
+    url: process.env.URL,
 }
