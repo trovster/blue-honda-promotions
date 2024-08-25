6414e65d70f11d43beb2a15fdb94de83c02c63fd
Trevor Morris <code@trovster.com>
2024-08-25 12:47:28 +0100

    fix: dynamically import functions

--- 11ty/functions/index.js
+++ 11ty/functions/index.js
@@ -1,11 +1,13 @@
-import dates from "./dates.js"
+import fs from "node:fs/promises"
 
-const functions = {
-    ...dates,
-}
+export default async (config) => {
+    const files = await fs.readdir(import.meta.dirname)
 
-export default (config) => {
-    for (const [name, fnc] of Object.entries(functions)) {
-        config.addJavaScriptFunction(name, fnc)
+    for (const file of files.filter((file) => file !== "index.js")) {
+        const functions = await import(import.meta.dirname + "/" + file)
+
+        for (const [name, fnc] of Object.entries(functions.default)) {
+            config.addJavaScriptFunction(name, fnc)
+        }
     }
 }
